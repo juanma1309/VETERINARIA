b69148302914aeedf5808a510f80a3ea3bc44a33
juanma1309 <juanmalopez12@yahoo.es>
2023-10-24 00:07:32 -0600

    base de datos

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use('/', require('./router'));
+
 app.listen(5000,()=>{
     console.log('server corriendo en http://localhost:5000')
 })
---
+++ router.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/contacto', (req, res)=>{
+    res.send('contacto');
+});
+
+
+
+module.exports = router
