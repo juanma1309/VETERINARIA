a667535854d6ad5999b2b9872940618c376efef2
juanma1309 <juanmalopez12@yahoo.es>
2023-10-23 23:58:34 -0600

    createmodules

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req,res)=>{
-    res.send('hola');
-})
+app.set('view engine', 'ejs');
 
 app.listen(5000,()=>{
     console.log('server corriendo en http://localhost:5000')
