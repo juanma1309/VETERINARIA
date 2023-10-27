b411b2f03b36a06f44320a78b42c8a0048d41554
juanma1309 <juanmalopez12@yahoo.es>
2023-10-26 20:55:48 -0600

    port

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.use(express.json());
 
 app.use('/', require('./router'));
 
-app.listen(5004,()=>{
-    console.log('server corriendo en http://localhost:5004')
-})
+// app.listen(5004,()=>{
+//     console.log('server corriendo en http://localhost:5004')
+// })
+
+app.set('port',process.env.PORT);
